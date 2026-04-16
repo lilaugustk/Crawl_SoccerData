6193c10574266eabf723010227ec7ba8e657034f
lilaugustk <lilaugustk@gmail.com>
2026-04-16 09:53:16 +0700

    oo

--- scrapers/fbref_match_scraper.py
+++ scrapers/fbref_match_scraper.py
@@ -8,8 +8,8 @@ import sys
 import gc
 
 class FBrefMatchScraper:
-    def __init__(self, start_date_str="1912-04-29", output_file="data/raw/fbref_all_matches/fbref_all_matches_1888_1950.csv"):
-    # def __init__(self, start_date_str="2024-01-01", output_file="data/raw/fbref_all_matches/fbref_all_matches_2024.csv"):
+    # def __init__(self, start_date_str="1912-04-29", output_file="data/raw/fbref_all_matches/fbref_all_matches_1888_1950.csv"):
+    def __init__(self, start_date_str="2026-01-01", output_file="data/raw/fbref_all_matches/fbref_all_matches_2026.csv"):
         self.start_date = datetime.strptime(start_date_str, "%Y-%m-%d").date()
         # self.end_date = date.today()
         self.end_date = date.today()
@@ -142,10 +142,10 @@ class FBrefMatchScraper:
 
 if __name__ == "__main__":
     # Bat dau tu dau nam 2026
-    scraper = FBrefMatchScraper(start_date_str="1912-04-29")
-    # scraper = FBrefMatchScraper(start_date_str="2024-01-01")
+    # scraper = FBrefMatchScraper(start_date_str="1912-04-29")
+    scraper = FBrefMatchScraper(start_date_str="2026-01-01")
     scraper.run()
     
-    sys.stderr = open(os.devnull, 'w')  
+    sys.stderr = open(os.devnull, 'w')
     del scraper 
     gc.collect()
